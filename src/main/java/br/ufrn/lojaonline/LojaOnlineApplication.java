198fa8d92518fb494f0766ca8ae7b8302e12cf57
Gustavo Souza de Lima <gustavo69gls@gmail.com>
2025-04-11 09:24:02 -0300

    novo projeto

---
+++ src/main/java/br/ufrn/lojaonline/LojaOnlineApplication.java
@@ -0,0 +1,13 @@
+package br.ufrn.lojaonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaOnlineApplication.class, args);
+	}
+
+}
