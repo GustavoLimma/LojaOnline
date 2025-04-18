8a96260253815134399000febd7c55842b89a1e2
Gustavo Souza de Lima <gustavo69gls@gmail.com>
2025-04-18 13:54:12 -0300

    correção

--- src/main/java/br/ufrn/lojaonline/repository/ProdutoRepository.java
+++
@@ -1,7 +0,0 @@
-package br.ufrn.lojaonline.repository;
-
-import br.ufrn.lojaonline.model.Produto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProdutoRepository extends JpaRepository<Produto, Long> {
-}
